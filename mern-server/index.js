fee91caf520f3461cc6526c3d2b8eea050686bb5
Nishit Gupta <gargnishit.2626@gmail.com>
2024-03-20 01:24:42 +0530

    images fixed

--- mern-server/index.js
+++ mern-server/index.js
@@ -65,7 +65,7 @@ async function run() {
             res.send(result)
         })
         
-        
+    
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
         
